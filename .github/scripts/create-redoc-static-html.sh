9be8906b126162d99af65306224eb7ea86ddb355
Anthony Lofton <anthony.lofton@jbhunt.com>
2024-10-07 16:34:13 -0500

    Updated the script to allow for multiple folders and automated scripting

--- .github/scripts/create-redoc-static-html.sh
+++ .github/scripts/create-redoc-static-html.sh
@@ -1,11 +1,59 @@
 #!/bin/bash
+set -e
 
-echo "Getting started"
+declare currentFolder=$(pwd)
+declare publicFolder="$currentFolder/public"
 
-# Bundle docs into zero-dependency HTML file
-npx redoc-cli bundle openapi.yaml && \
-mv redoc-static.html index.html && \
-echo "Changed name from redoc-static.html to index.html" && \
-# Add favicon
-sed -i '7 i \ \ <link rel="icon" type="image/png" href="images/favicon.png"/>' index.html && \
-echo -e "\nDone!"
+# Creates static html files for openapi.yaml file in the current directory
+loadStaticHtmlToFolder() {
+    local folder="$1"
+    echo "Creating folder $publicFolder/$folder"
+    mkdir -p $publicFolder/$folder
+    npx redoc-cli bundle $currentFolder/$folder/openapi.yaml -o $publicFolder/$folder/index.html
+}
+
+# Selects all folders in the current directory
+selectAllFoldersInDirectory() {
+    local -n folders=$1
+    echo "Selecting all folders in $currentFolder"
+    
+    # Use glob pattern to get all directories
+    for dir in "$currentFolder"/*/; do
+        # Check if it's a directory
+        if [ -d "$dir" ] && [[ "$dir" != */images/ ]] && [[ "$dir" != */public/ ]]; then
+            folders+=("$dir")
+        fi
+    done
+
+    # Stripping the path and getting only the folder name
+    folders=("${folders[@]%/}")
+    folders=("${folders[@]##*/}")
+}
+
+# Initializes the public folder
+initializePublicFolder() {
+    echo "Initializing public folder"
+    mkdir -p $publicFolder
+    cp -r $currentFolder/images $publicFolder
+}
+
+# Creates index.html file
+createIndexHtml() {
+    local -n folders=$1
+    echo "Creating index.html"
+    local indexHtml="$publicFolder/index.html"
+    echo "<html><head><title>SSC</title></head><body><h1>SSC</h1><ul>" > $indexHtml
+    for folder in "${folders[@]}"; do
+        echo "<li><a href=\"$folder/index.html\">$folder</a></li>" >> $indexHtml
+    done
+    echo "</ul></body></html>" >> $indexHtml
+}
+
+declare allFolders
+selectAllFoldersInDirectory allFolders
+initializePublicFolder
+createIndexHtml allFolders
+
+for directory in "${allFolders[@]}"; do
+    loadStaticHtmlToFolder $directory
+done
